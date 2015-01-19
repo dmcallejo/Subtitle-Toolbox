12eed60be20772dfc2f2bedf1eda6d174fa5287c
dmcelectrico <dmcelectrico@gmail.com>
2015-01-19 21:22:01 +0100

    Command line options.
    Also beginning of the filename option implementation.

--- subtitle_toolbox.py
+++ subtitle_toolbox.py
@@ -3,10 +3,74 @@
 
 import sites.subtitulos_es
 import urllib2
-import sys
+import sys,math,getopt,re
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
+def main(argv):
+	video_file = None
+	tvseries = None
+	season = None
+	episode = None
+	try:
+		opts, args = getopt.getopt(argv,"hf:t:s:e:",["test","tvseries=","season=","episode=","file="])
+	except getopt.GetoptError:
+		print 'subtitle_toolbox.py -t <tv-series> -s <season> -e <episode>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'subtitle_toolbox.py -t <tv-series> -s <season> -e <episode>'
+			sys.exit()
+		elif opt in ("-f", "--file"):
+			video_file=arg
+			break
+		elif opt in ("-t", "--tvseries"):
+			tvseries = arg
+			continue
+		elif opt in ("-s", "--season"):
+			season = int(arg)
+		elif opt in ("-e", "--episode"):
+			episode = int(arg)
+		elif opt in ("--test"):
+			test()
+			exit(0)
+
+	if(video_file!=None):
+		download_by_file(video_file)
+		exit(0)
+	if(tvseries==None):
+		print "No series specified."
+		sys.exit(2)
+	elif(season==None or math.isnan(season)):
+		print "No season specified."
+		sys.exit(2)
+	elif(episode==None or math.isnan(episode)):
+		print "No season specified."
+		sys.exit(2)
+
+	sites.subtitulos_es.get_all_subtitles(tvseries,season,episode)
+	exit(0)
+
+
+def download_by_file(video_file):
+	if(video_file.rfind("/")!=-1):
+		path,filename = video_file.rsplit("/",1)
+	else:
+		path=""
+		filename=video_file
+	season = re.search('(?<=[Ss])[0-9][0-9](?=E[0-9][0-9])',filename).group(0)
+	episode = re.search('(?<=S[0-9][0-9]E)[0-9][0-9]',filename).group(0)
+	
+
+	print season
+	print episode
+	print path
+	print filename
+
+
 def test():
-	sites.subtitulos_es.test()
+	sites.subtitulos_es.get_all_subtitles("Banshee",3,1)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
--- utils.py
+++ utils.py
@@ -27,6 +27,9 @@ def sanitize_show_name(show,debug=False):
 def get_soup_from_URL(url,debug=False):
 	return BeautifulSoup(get_page_from_URL(url,debug=False))
 
+def parse_tvseries_filename(file_name):
+	""" TODO """
+	pass
 
 
 MODULE_EXTENSIONS = ('.py', '.pyc', '.pyo')
@@ -39,3 +42,25 @@ def package_contents(package_name):
 	return set([os.path.splitext(module)[0]
 		for module in os.listdir(pathname)
 		if module.endswith(MODULE_EXTENSIONS)])
+
+def download_file(url,file_name):
+	u = urllib2.urlopen(url)
+	f = open(file_name, 'wb')
+	meta = u.info()
+	file_size = int(meta.getheaders("Content-Length")[0])
+	print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+	file_size_dl = 0
+	block_sz = 8192
+	while True:
+		buffer = u.read(block_sz)
+		if not buffer:
+			break
+
+		file_size_dl += len(buffer)
+		f.write(buffer)
+		status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+		status = status + chr(8)*(len(status)+1)
+		print status,
+
+	f.close()
