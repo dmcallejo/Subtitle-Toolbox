8b997cd0c6021339a2de6e84f0ebdaa3145fb15d
dmcelectrico <dmcelectrico@gmail.com>
2016-08-05 00:54:31 +0200

    tvdb imdb ids retrieval

--- sites/tvdb.py
+++ sites/tvdb.py
@@ -12,12 +12,23 @@ class tvdb:
 
         def get_info(self,series,season,episode):
                 print("tvdb: Searching",series,"S"+season+"E"+episode)
+                #Searching for the tv show first
+                try:
+                        search_result = self.t[series]
+                except tvdb_exceptions.tvdb_shownotfound as e:
+                        print("tvdb: tv show not found:",series)
+                        return None
+                
+                series_imdb_id = search_result['imdb_id']
+
+                #Episode search now
                 try:
                         search_result = self.t[series][int(season)][int(episode)]
                 except tvdb_exceptions.tvdb_episodenotfound as e:
                         print("tvdb: Episode not found:",series,"S"+season+"E"+episode)
                         return None
-                #TODO
-                episode_info = Episode(series=series,season=season,episode=episode,title=search_result['episodename'])
+                episode_name = search_result['episodename']
+                episode_imdb_id = search_result['imdb_id']
+                episode_info = Episode(series=series,season=season,episode=episode,episode_name=episode_name,series_imdb_id=series_imdb_id,episode_imdb_id=episode_imdb_id)
                 return episode_info
 
